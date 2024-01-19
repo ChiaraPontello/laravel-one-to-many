7197741cfa5b572fae3c40cba3b878720b5a175e
ChiaraPontello <chiarapontello1@gmail.com>
2024-01-19 16:08:57 +0100

    routes category

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -5,13 +5,13 @@
         <p>{{$category->body}}></p>
         <img src="{{asset('storage/' . $category->image)}}" alt="{{$category->name}}">
         <div class="d-flex">
-            <form action="{{ route('admin.categories.destroy', $category->id)}}" method="POST">
+            <form action="{{ route('admin.categories.destroy', $category->slug)}}" method="POST">
             @csrf
             @method('DELETE')
             <button type="submit" data-item-title="{{$category->name}}" class="cancel-button btn btn-danger">
             Delete</button>
             </form>
-            <a class="btn btn-primary ms-2" href="{{route('admin.categories.edit', $category->id)}}">Edit</a>
+            <a class="btn btn-primary ms-2" href="{{route('admin.categories.edit', $category->slug)}}">Edit</a>
             <a class="btn btn-warning ms-2" href="{{route('admin.categories.index')}}">Back</a>
         </div>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\CategoryController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +27,9 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class)->parameters([
         'projects' => 'project:slug'
+    ]);
+    Route::resource('categories', CategoryController::class)->parameters([
+        'categories' => 'category:slug'
     ]);
 });
 
